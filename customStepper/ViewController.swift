5956434e6ab10ccaeb6ac372d309c6af1547d6df
Tyler Yan <tylerjjyan@gmail.com>
2015-03-27 16:23:40 -0700

    Initial Commit

---
+++ customStepper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customStepper
+//
+//  Created by Tyler Yan on 2015-03-27.
+//  Copyright (c) 2015 Foodee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
